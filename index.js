31d011b73c74d247b7cde3c2abd076cb59062b75
Chalermrat Choeisai <schalermrat@gmail.com>
2017-12-18 14:31:59 +0700

    feat(numbers): add addition multiple numbers
    Closes: #2 BREAKING CHANGE: add2Numbers()

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const add2Numbers = (a,b) => {
-  return a + b;
+  console.warn(`add2Numbers()
+ will deprecated, please use addNumbers() instead`);
+
+  return addNumbers(a, b);
+}
+
+const addNumbers = (...numbers) => {
+  return numbers.reduce((a, b) => a + b);
 }
